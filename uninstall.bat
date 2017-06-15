d90f81e682ca07ddca522f5a67349e8898f75ff6
Sam <lonegather@users.noreply.github.com>
2017-06-15 15:59:26 +0800

    Delete uninstall.bat

--- uninstall.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-::set regpath = HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment
-::set evname = MAYA_MODULE_PATH
-setx MAYA_MODULE_PATH "" /M
-::setx MAYA_SHELF_PATH "" /M
-setx MAYA_SCRIPT_PATH "" /M
-setx PYTHONPATH "" /M
