ac85c39cbfbf8f991db255c4f9975f04eb6a22e1
Sam <562692048@qq.com>
2018-02-23 14:59:07 +0800

    improved

--- commons/scripts/barbarian/reloader.py
+++ commons/scripts/barbarian/reloader.py
@@ -10,7 +10,12 @@ import os
 import sys
 from maya import cmds
 
-ignore_list = ['reloader.py', 'blend.py', 'pathAdd.py']
+ignore_list = [
+    'barbarian',
+    'barbarian.reloader',
+    'barbarian.rig.blendShapeMel.blend',
+    'barbarian.rig.blendShapeMel.pathAdd',
+]
 
 
 def doIt(rl=False):
@@ -29,12 +34,12 @@ def doIt(rl=False):
     for root, dirs, files in os.walk(base):
         for f in files:
             if f[-3:] == '.py':
-                if f in ignore_list: continue
                 root = root.replace('\\', '/')
                 rel_path = os.path.join(root, f).replace('\\', '/').split(base)[-1][:-3].split('/')
                 if rel_path[-1] == '__init__':
                     rel_path = rel_path[:-1]
                 mod = '.'.join(rel_path)
+                if mod in ignore_list: continue
                 try:
                     __import__(mod)
                     if rl: reload(sys.modules[mod])
